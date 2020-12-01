e7842eb5d553c0aa712004e79c25ea60e1bcd1d4
Cody Dow <wodydoc@gmail.com>
2020-12-01 18:31:42 -0500

    removing profile image animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,4 +65,4 @@ gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
 gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
 
 // IMG
-gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
+// gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
